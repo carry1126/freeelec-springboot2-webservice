ad726e38b1ce87132f6b5dd21cede65e6cc6b8f8
minjikim <carrykim1126@gmail.com>
2020-07-17 22:47:24 +0900

    adding Testcode and related JPA

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -1,11 +1,21 @@
 package com.jojoldu.book.springboot;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.Entity;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @Entity
+    public static class Posts {
     }
 }
